f884227bdbed0cbf0a909a1f285e269de2283df0
Bard-Gaming <christophe@dronne.fr>
2026-06-16 14:39:25 +0200

    feat(concepts): add utility header file for concepts module

---
+++ include/lattice/concepts.hpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2026
+** Project - Lattice
+** File description:
+** Utility header file for
+** concepts used by lattice
+*/
+
+#pragma once
+
+#include "../../src/concepts/Serializable.hpp"
+#include "../../src/concepts/SocketType.hpp"
